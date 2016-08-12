b9bc420bb5701e1c314db2b3933d9a876c5560e8
dhferdian <dferdian@booster.com>
2016-08-13 00:02:56 +0800

    Basic routes and dynamically load controller
    This commit try to implement very basic routing from http request to
    controller.

--- lib/application.rb
+++ lib/application.rb
@@ -1,9 +1,24 @@
+require "app/controllers/application_controller"
+
 class Application
   def call(env)
     request = Rack::Request.new(env)
     response = Rack::Response.new
 
-    response.write "Hello Ruby!"
+    controller_name, action_name = route(request.path_info)
+    controller_class = load_controller_class(controller_name)
+
     response.finish
+  end
+
+  def route(path)
+    # path = "/home/index" => "". "home", "index"
+    _, controller, action = path.split("/")
+    [ controller || "home", action || "index" ]
+  end
+
+  def load_controller_class(name)
+    require "app/controllers/#{name}_controller"
+    Object.const_get(name.capitalize + "Controller")
   end
 end
