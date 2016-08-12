6495351295d9ca3ef75c1aabf95644e6a6c9ff85
dhferdian <dferdian@booster.com>
2016-08-12 23:03:42 +0800

    Basic User class to pass UserTest test case
    User class build based on UserTest case which implement very basic
    response to the unit test to make it passed. A few places still very
    static and we will address it in next few commits.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,36 @@
-class User
+require "connection_adapter"
 
+class User
+  @@connection = SqliteAdapter.new
+
+  def initialize(attributes)
+    @attributes = attributes
+  end
+
+  def id
+    attributes[:id]
+  end
+
+  def name
+    attributes[:name]
+  end
+
+  def self.find(id)
+    find_by_sql("SELECT * FROM users WHERE id = #{id.to_i} LIMIT 1").first
+  end
+
+  def self.all
+    find_by_sql("SELECT * FROM users")
+  end
+
+  private
+
+  def self.find_by_sql(sql)
+    rows = @@connection.execute(sql)
+    rows.map do |attribute|
+      new(attribute)
+    end
+  end
+
+  attr_reader :attributes
 end
